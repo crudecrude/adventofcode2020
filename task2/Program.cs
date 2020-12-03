da8b0b67b0c2777940a15db2e2a4fb83aedcd7df
Roman Taborsky <crudecrude@gmail.com>
2020-12-03 16:39:40 +0100

    task 3 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace task2
 {
@@ -6,7 +7,43 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = System.IO.File.ReadAllLines("input.txt");
+
+            //
+
+            //3-10 v: vvgvvvvvvtvvvvh
+
+            var passwords = lines.Select(x => {
+                var a = x.Split(' ');
+                
+                return new Tuple<int, int, char, string>(Int32.Parse(a[0].Split('-')[0]),Int32.Parse(a[0].Split('-')[1]), a[1][0],a[2].Trim());
+            });
+            int correctPasswords = 0;
+            foreach (var password in passwords)
+            {
+                var charCount = 0;
+                for(int i=0;i<password.Item4.Length;i++)
+                {
+                    if (password.Item4[i] == password.Item3) charCount++;
+                }
+                if (password.Item1 <= charCount && charCount <= password.Item2)
+                    correctPasswords++;
+            }
+            Console.WriteLine(correctPasswords);
+            correctPasswords = 0;
+            foreach (var password in passwords)
+            {
+                var contains1 = (password.Item4[password.Item1-1] == password.Item3);
+                var contains2 = (password.Item4[password.Item2-1] == password.Item3);
+                if (contains1 && contains2) continue;
+                if (!contains1 && !contains2) continue;
+
+                correctPasswords++;
+            }
+                
+                    
+            
+            Console.WriteLine(correctPasswords);
         }
     }
 }
---
+++ task3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task3
+{
+    class Program
+    {
+        class vectorMap
+        {
+            public int rightVector;
+            public int downVector;
+            public int position;
+            public int trees;
+        }
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines("input.txt").ToArray();
+            var position = 0;
+            var downVector = 1;
+            var rightVector = 3;
+
+            var lineLength = lines[0].Length;
+
+            var trees = 0;
+            for (int i = 0; i < lines.Length; i = i + downVector)
+            {
+                if (lines[i][position] == '#') trees++;
+                position = (position + rightVector) % lineLength;
+            }
+            Console.WriteLine(trees);
+
+            /*
+            Right 1, down 1.
+            Right 3, down 1. (This is the slope you already checked.)
+            Right 5, down 1.
+            Right 7, down 1.
+            Right 1, down 2
+             */
+            //right,down,position,trees
+
+            var x = new List<vectorMap>();
+            x.Add(new vectorMap() { rightVector = 1, downVector = 1, position = 0, trees = 0 });
+            x.Add(new vectorMap() { rightVector = 3, downVector = 1, position = 0, trees = 0 });
+            x.Add(new vectorMap() { rightVector = 5, downVector = 1, position = 0, trees = 0 });
+            x.Add(new vectorMap() { rightVector = 7, downVector = 1, position = 0, trees = 0 });
+            x.Add(new vectorMap() { rightVector = 1, downVector = 2, position = 0, trees = 0 });
+
+            for (int i = 0; i < lines.Length; i = i + downVector)
+            {
+                foreach (var vector in x)
+                {
+                    if (i % vector.downVector == 0)
+                    {
+                        if (lines[i][vector.position] == '#') vector.trees++;
+                        vector.position = (vector.position + vector.rightVector) % lineLength;
+                    }
+
+                }
+
+            }
+            Int64 result = 0;
+            foreach (var vector in x)
+            {
+                if (result == 0) { 
+                    result = vector.trees;
+                } else { 
+                    result = result * vector.trees;
+                };
+
+                
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
